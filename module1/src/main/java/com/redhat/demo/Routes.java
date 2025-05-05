3773420c7d732dc057b9b4df913c9ff31026936d
Sumit Mukherjee <sumit.mckv@gmail.com>
2025-05-05 20:33:47 +0530

    first commit

---
+++ module1/src/main/java/com/redhat/demo/Routes.java
@@ -0,0 +1,13 @@
+package com.redhat.demo;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class Routes extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:foo?period=1000")
+        .log("Welcome to Camel Workshop");
+    }
+    
+}
