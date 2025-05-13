250078d79f9976af95207fee19065127de12500f
Sumit Mukherjee <sumit.mckv@gmail.com>
2025-05-13 10:41:38 +0530

    Added code to write message to Kafka

---
+++ module5/src/main/java/com/redhat/demo/Routes.java
@@ -0,0 +1,42 @@
+package com.redhat.demo;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Exchanger;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.model.rest.RestBindingMode;
+
+public class Routes extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+ 
+        restConfiguration().scheme("http");
+        
+        rest("/lead")
+        .post()
+        .wiretap("direct:postLeadToKafka")
+        .setBody(constant("\{\"status\":\"Sent input request to Kafka. Expect response shortly.\"\}"));
+
+        from("direct:postLeadToKafka")
+        .to("kafka:{{kafka.topic.name}}");
+
+        // from("direct:postLead")
+        // .setHeader(Exchange.CONTENT_TYPE, simple("application/json"))
+        // .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+        // .setHeader(Exchange.HTTP_PATH, constant("/lead"))
+        // .to("{{LEAD_BACKEND_ADDRESS}}?bridgeEndpoint=true")
+       
+        // convert the body back to XML
+        // .to("xj:json2xml-lead.xsl?transformDirection=JSON2XML")
+        // .setHeader(Exchange.CONTENT_TYPE, constant("application/xml"));
+
+        // Kafka consumer
+        from("kafka:{{kafka.topic.name}}")
+        .log("Received : \"${body}\"");
+
+    }
+    
+}
---
+++ module5/src/main/java/com/redhat/demo/input.java
@@ -0,0 +1,18 @@
+package com.redhat.demo;
+
+public class input {
+    private String clientid;
+    private String clientsecret;
+    public String getClientid() {
+        return clientid;
+    }
+    public void setClientid(String clientid) {
+        this.clientid = clientid;
+    }
+    public String getClientsecret() {
+        return clientsecret;
+    }
+    public void setClientsecret(String clientsecret) {
+        this.clientsecret = clientsecret;
+    }
+}
